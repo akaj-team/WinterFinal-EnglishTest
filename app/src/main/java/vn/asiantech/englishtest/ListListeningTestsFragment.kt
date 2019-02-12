214c5e8dbc96d14ee5d7685412885a413a9bb90e
Bao Le Q <baole@asiantech.vn>
2019-02-12 10:39:16 +0700

    Add drawable and init fragment

---
+++ app/src/main/java/vn/asiantech/englishtest/ListListeningTestsFragment.kt
@@ -0,0 +1,17 @@
+package vn.asiantech.englishtest
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ListListeningTestsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list_listening_tests, container, false)
+    }
+}
--- app/src/main/java/vn/asiantech/englishtest/MainActivity.kt
+++ app/src/main/java/vn/asiantech/englishtest/MainActivity.kt
@@ -1,12 +1,22 @@
 package vn.asiantech.englishtest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFragment()
+    }
+
+    private fun initFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.listListeningTestsFragment, ListListeningTestsFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
